dd6ee82b2668185499f8eddc0a1d1e1949aba66f
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-09-01 22:46:24 +0530

    database

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 // step 1 :- connect to mongodb
 const connect = () => {
   return mongoose.connect(
-    "mongodb://127.0.0.1:27017/spotify"
-    // "mongodb+srv://zaki:zaki_123@spotify.fpdhgpr.mongodb.net/?retryWrites=true&w=majority"
+    // "mongodb://127.0.0.1:27017/spotify"
+    "mongodb+srv://zaki:zaki_123@spotify.fpdhgpr.mongodb.net/?retryWrites=true&w=majority"
   );
 };
 
